a167ce01064c6e3695af6f1312f2bdeb0de76844
Ingo F <ingo.fruend@twentybn.com>
2016-11-18 14:54:18 +0100

    creating url

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tnt
+
 tnt is not tensorflow
+
+
+We find it pretty annoying that you can't just
+
+    pip install tensorflow
+
+For that reason, we now suggest to run
+
+    pip install tnt
--- setup.py
+++ setup.py
@@ -1,1 +1,67 @@
-print('dummy')
+import sys
+import os
+from distutils.core import setup
+
+
+class UnsupportedPlatformException(Exception):
+    pass
+
+
+def install_tensorflow():
+    # are we python3 or python2
+    # do we have a cpu
+    # mac or linux
+    pass
+
+
+def pythonversion():
+    return sys.version[:2]
+
+
+def hasgpu():
+    return not os.system('which nvidia-smi')
+
+
+def platform():
+    platform_name = sys.platform
+    if platform_name.startswith('linux'):
+        return 'linux'
+    elif platform_name.startswith('darwin'):
+        return 'mac'
+    else:
+        raise UnsupportedPlatformException(
+            'Only linux and mac are supported, but you are on "{}"'
+            .format(platform_name))
+
+
+def create_fuzzyness(platform_name, python_version):
+    if platform_name == 'mac':
+        return 'py{}-none-any'.format(python_version[0])
+    elif platform_name == 'linux':
+        cpstring = '{}{}'.format(*python_version)
+        if cpstring == '27':
+            return 'cp27-none-linux_x86_64'
+        else:
+            return 'cp{}-cp{}m-linux_x86_64'.format(cpstring, cpstring)
+
+
+def build_url(tf_version, python_version, processing_unit, platform_name):
+    kwargs = {
+        'platform_name': platform_name,
+        'processing_unit': processing_unit,
+        'tf_version': tf_version,
+        'kladderadatsch': create_fuzzyness
+    }
+    return ('https://storage.googleapis.com/tensorflow/'
+            '{platform_name}/{processing_unit}/'
+            'tensorflow-{tf_version}-{kladderadatsch}.whl'
+            .format(**kwargs))
+
+
+if __name__ == '__main__':
+    install_tensorflow()
+
+    setup(
+        name='tnt',
+        version='0.0.0',
+    )
